b1cdf789e3c8c18caae7c4dc1cdbd4928eeb0be7
PratibhaR07 <pratibharathod611@gmail.com>
2026-04-08 00:20:11 +0530

    Added BaseTest class for Selenium WebDriver setup

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.flipkart.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
