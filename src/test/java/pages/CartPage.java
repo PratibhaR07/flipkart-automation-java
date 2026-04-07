48f5eed09ddad614b7e86102411401b10a37deb3
PratibhaR07 <pratibharathod611@gmail.com>
2026-04-08 00:23:32 +0530

    Added CartPage class for cart operations

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTotalAmount() {
+        return driver.findElement(By.xpath("//span[contains(text(),'Total Amount')]")).getText();
+    }
+
+    public void increaseQty() {
+        driver.findElement(By.xpath("//button[contains(text(),'+')]")).click();
+    }
+
+    public void removeItem() {
+        driver.findElement(By.xpath("//div[text()='Remove']")).click();
+        driver.findElement(By.xpath("//div[text()='Remove']")).click();
+    }
+
+    public boolean isCartEmpty() {
+        return driver.getPageSource().contains("Missing Cart items");
+    }
+}
